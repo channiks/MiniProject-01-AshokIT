525d476c2699665eef61811cd3d71c84ca71e366
channiks.name <nikhilmohanty20@gmail.com>
2022-04-22 10:13:56 +0530

    make it better

---
+++ src/main/java/in/ashokit/entity/Contact.java
@@ -0,0 +1,55 @@
+package in.ashokit.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//import lombok.Data;
+
+@Entity
+@Table(name="CONTACT_DTLS")
+
+public class Contact {
+	
+	@Id
+	@GeneratedValue
+	private int cid;
+	private String name;
+	private String email;
+	private long phno;
+	private String ativeSw;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	public String getAtiveSw() {
+		return ativeSw;
+	}
+	public void setAtiveSw(String ativeSw) {
+		this.ativeSw = ativeSw;
+	}
+	
+	
+	
+}
